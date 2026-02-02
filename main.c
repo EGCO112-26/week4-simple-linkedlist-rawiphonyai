79aac5b40f8dd13d8ffb8b635cec7e9f9efbe2ca
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-02-02 11:09:33 +0000

    Assignment 4

--- main.c
+++ main.c
@@ -1,63 +1,40 @@
-//
-//  main.c
-//  simple linkedlist
-//
-//  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "node.h"
+#include <string.h>
+#include "std_node.h"
 
 int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
-    a.next=&b;
-    head=&a;
-    b.value=head->value+3;
+    std_NodePtr head,temp;
+    head = (std_NodePtr)malloc(sizeof(struct std_node));
+    temp = head;
+    int i,n=(argc-1)/2;
 
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
-/*  Exercise I
-    1. Add 1 more than at the end
-    2. Add value(11)
-    3. Make next become NULL
- */
-    
-/*  Exercise II
-        1. Add 1 more than at the begining!!!!
-        2. Add value (2)
-        
-*/
-    typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-          // What is missing???
-        }
-    */
-    
-   /*  Exercise IV change to while loop!! (you can use NULL to help)
-       
-         while(){
-            printf("%3d", tmp->value);
-           // What is missing???
-        }
-    */
-    
- /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
-         //use a loop to help
-          
-     */
+    for(i=0;i<n-1;i++){
+        temp->id = atoi(argv[2*i+1]);
+        strcpy(temp->name ,argv[2*i+2]);
+        temp->next = (std_NodePtr)malloc(sizeof(struct std_node));
+        temp=temp->next;
+    }
 
-    /*  Exercise VI Free all node !!
-         //use a loop to help
-          
-     */
-    
+    temp->id = atoi(argv[2*i+1]);
+    strcpy(temp->name ,argv[2*i+2]);
+    temp->next=NULL;
+
+    printf("Student List :\n");
+    temp = head;
+    while(temp){
+        printf("ID : %d", temp->id);
+        printf("  Name : %s\n", temp->name);
+        temp=temp->next;
+    }
+
+    temp = head;
+    while(temp){
+        head = head->next;
+        free(temp);
+        temp = head;
+    }
+    head = NULL;
+
     return 0;
 }
